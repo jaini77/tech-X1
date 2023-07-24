68598389701c5cc067bf63e4732144061cb9e33e
= <=>
2023-07-24 18:00:44 +0530

    second

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-firebse-app',
+    appId: '1:198958285098:web:84768afad4a868e51d5e53',
+    storageBucket: 'angular-firebse-app.appspot.com',
+    apiKey: 'AIzaSyCDhjpoWBWGG2IUkBJjnOwaiQTh6OgiEcc',
+    authDomain: 'angular-firebse-app.firebaseapp.com',
+    messagingSenderId: '198958285098',
+    measurementId: 'G-CGLNXQLP56',
+  },
+};
